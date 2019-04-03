6c394c1361fe00b73569fef917a2fb47aeeff889
igorbrehm-lis <igor.brehm@pucrs.br>
2019-04-03 12:59:37 -0300

    melhorias na logica do leilao

--- README.md
+++
@@ -1,1 +0,0 @@
-# Quero ir pra casaa!!!
--- TCP_Server.java
+++ TCP_Server.java
@@ -7,7 +7,17 @@ class TCPServer {
 		String clientSentence;
 		ServerSocket welcomeSocket = new ServerSocket(6789);
 		int bestBet = 0;
+		int num_bets = 10;
 		while (true) {
+			if(num_bets == 0){
+				System.out.println("\nLeilao terminou!");
+				System.out.println("Lance Vencedor: "+ bestBet);
+				break;
+			}
+			else{
+				System.out.println("\nLances Restantes: "+ num_bets);
+				num_bets = num_bets - 1;	
+			}
 			Socket connectionSocket = welcomeSocket.accept();
 			BufferedReader inFromClient =
 			new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
@@ -15,7 +25,7 @@ class TCPServer {
 			System.out.println("Lance Recebido: " + clientSentence);
 			if(Integer.parseInt(clientSentence) > bestBet){
 				bestBet = Integer.parseInt(clientSentence);
-				System.out.println("Maior lance ate agora!");
+				System.out.println("\nMaior lance ate agora!");
 			}
 		}
 	}
