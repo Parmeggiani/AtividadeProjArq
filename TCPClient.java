2d8582cf662b265f524542d0233c8c13b5a41397
IgorBrehm <igorbrehm@hotmail.com>
2019-04-05 20:08:44 -0300

    Add files via upload

--- TCPClient.java
+++ TCPClient.java
@@ -3,21 +3,23 @@ import java.net.*;
 import java.util.Scanner;
 
 //Alunos: Alexandre Bing, Igor Brehm e Vinícius Parmeggiani
-
+// Implementar um banco de dados e realizar a comunicacao durante a execucao do servidor
 class TCPClient {
 	public static void main(String args[]) throws Exception {
 	  	Scanner input = new Scanner (System.in);
 		String message;
+		String name;
+		System.out.println("Digite seu nome:");
+        name = input.nextLine();
 		boolean flag = false;
 		while (flag == false) {
-            System.out.println("Digite seu nome:");
-            message = input.nextLine();
-            if (message.equals("sair")) {
+			System.out.println("\nDigite seu lance:");
+			message = input.nextLine();
+			if (message.equals("sair")) {
 				flag = true;
 				break;
 			}
-			System.out.println("Digite seu lance:");
-			message = message + "-"+ input.nextLine();
+			message = name + "-"+ message;
 			Socket clientSocket = new Socket("localhost", 6789);
 			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 			System.out.println("Para fechar o programa, digite \'sair\'");
